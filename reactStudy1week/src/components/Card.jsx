8690dff4c5f886111c60de9ef21fc8255be68061
Beongu Cho <whqjarn111@gcdc.co.kr>
2025-05-09 22:11:30 +0900

    출퇴근 버튼 생성, 9시 이후 지각표시, 출근시간 localStorage 저장 기능 추가가

--- reactStudy1week/src/components/Card.jsx
+++ reactStudy1week/src/components/Card.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Card(props) {
+    const [isWorking, setIsWorking] = useState(false)
+    const [isLate, setIsLate] = useState(false)
     const memberInfo = props.memberInfo;
     const nowDate = new Date();
     const joinDate = new Date(memberInfo.joinDate);
@@ -17,7 +19,30 @@ function Card(props) {
     if (memberInfo.department === "해외영업팀") {
         memberInfo.backgroundColor = "bg-red-200"
     }
-    
+    // 출퇴근 상태 변경
+    function changeWorkingState(id) {
+        // isWorking == false -> 퇴근 상태
+        // isWorking == true -> 출근 상태
+        const currentHour = new Date();
+        setIsWorking(!isWorking);
+        localStorage.setItem(id, currentHour)
+        
+        // 9시 이후 출근 시 지각 표시
+        if (!isWorking && currentHour.getHours() >= 9 && currentHour.getMinutes() >= 0) {
+            setIsLate(true);
+        }
+
+        if (isWorking) {
+            setIsLate(false)
+        }
+    }
+    // 출퇴근 시간 localStorage 저장
+    function getCheckedHour(id) {
+        return localStorage.getItem(id) 
+    }
+    // Uncaught Error: Too many re-renders. React limits the number of renders to prevent an infinite loop.
+    // state를 변화시키는 함수가 무한루프에 빠지게 되면 발생하는 에러로 볼 수 있음
+    // state를 변화시키는 함수가 사용자 액션 시에만 동작하도록 해야함 -> 특히 이벤트 트리거에 연관된 함수
     return (
         <div className="max-w-sm w-full lg:max-w-full lg:flex pb-3">
             <div className="h-48 lg:h-auto w-128 lg:w-128 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden">
@@ -40,13 +65,26 @@ function Card(props) {
                                 fieldName = "년차"
                             }
                             return (
-                                fieldName === '' ? false:
-                                <p className="text-base text-black-600 flex items-center" key={key + memberInfo.name}>
-                                {fieldName}: {memberInfo[key]}
-                                </p>
+                                <div key={key + memberInfo.name}>
+                                    {
+                                        fieldName === '' ? <></> :
+                                        <p className="text-base text-black-600 flex items-center" >
+                                            {fieldName}: {memberInfo[key]}
+                                        </p>
+                                    }
+                                </div>
                             )
                         })
                     }
+                    <div>
+                        <p className="text-base text-black-600 flex items-center">
+                            {isWorking ? '출근 완료' : '퇴근 완료'} {isLate ? '지각입니다' : ''}
+                        </p>
+                        <p className="text-base text-black-600 flex items-center">
+                            {isWorking ? '출근시간: ' : '퇴근시간: '} {getCheckedHour(memberInfo.id)}
+                        </p>
+                    </div>
+                    <button onClick={() => changeWorkingState(memberInfo.id)} className="" >{isWorking ? '퇴근' : '출근'}</button>
                 </div>
             </div>
             <div className="h-48 lg:h-auto w-128 lg:w-128 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden">
