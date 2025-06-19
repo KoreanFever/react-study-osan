bf40d4baf204042e486257a40a9f2d6b5aadd0f8
Beongu Cho <whqjarn111@gcdc.co.kr>
2025-06-20 02:58:45 +0900

    연차 신청 폼 작성

--- reactStudy2week/src/components/VacationRequest.jsx
+++ reactStudy2week/src/components/VacationRequest.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 /**
  * 핵심 키워드 - 폼 구성 / 입력 관리 / 조건부 렌더링
  * 공통 과제 - 연차 반차 반반차 선택 가능한 폼 만들기
@@ -10,7 +10,7 @@ function VacationRequest(props) {
         vacationTimeType:'',
         vacationRangeType: '',
         vacationDate: '',
-        vacationMemberId: -1 
+        vacationUserName: ''
     }
     const vacationTimeType = [
         {name: '오전', value: 'am'},
@@ -22,15 +22,18 @@ function VacationRequest(props) {
         {name: '연차', value: 'full'}
     ]
     const [params, setParams] = useState(initParams) 
-    const [isSelectableVacationTime, setIsSelectableVacationTime] = useState(false)
+    const [disableSelectableVacationTime, setDisableSelectableVacationTime] = useState(false)
 
+    // useEffect(() => {
+    //     console.log('isSelectableVacationTime', disableSelectableVacationTime)
+    // }, [disableSelectableVacationTime])
     const changeVacationRangeType = (e) => {
         const type = e.target.value
         if (type === 'full') {
-            setIsSelectableVacationTime(false)
+            setDisableSelectableVacationTime(true)
             setParams({...params, vacationTimeType: ''})
         } else {
-            setIsSelectableVacationTime(true)
+            setDisableSelectableVacationTime(false)
         }
         setParams({...params, vacationRangeType: type})
         console.log(params)
@@ -38,6 +41,19 @@ function VacationRequest(props) {
     const changeVacationTimeType = (e) => {
         const type = e.target.value
         setParams({...params, vacationTimeType: type})
+    }
+    const changeVacationUserName = (e) => {
+        const vacationUserName = e.target.value
+        setParams({...params, vacationUserName: vacationUserName})
+    }
+    const changeVacationDate = (e) => {
+        const vacationDate = e.target.value
+        setParams({...params, vacationDate: vacationDate})
+    }
+
+    const submitVacationRequest = (event) => {
+        event.preventDefault()
+        console.log(params)
     }
 
     return (
@@ -47,44 +63,70 @@ function VacationRequest(props) {
             <form className="w-full" onSubmit={submitVacationRequest}>
                 <table className="border border-gray-300 w-full table-auto">
                     <tbody>
-                <tr>
+                        <tr>
                             <th scope="row" className="border border-gray-300 w-1/3">시간</th>
                             <td className="border border-gray-300 w-2/3">
-                        {vacationTimeType.map((vacationTime, idx) => {
-                            return (
-                                <div className="float-start" key={'vacationTimeType' + idx}>
-                                    <input 
-                                        type="radio" 
-                                        name="vacationTimeType"
-                                        value={vacationTime.value}
-                                        onClick={changeVacationTimeType}
-                                        disabled={isSelectableVacationTime}
-                                    />
-                                    <label htmlFor={vacationTime.name}>{vacationTime.name}</label>
-                                </div>
-                                )
-                        })} 
-                    </td>
-                </tr>
-                <tr>
+                                {vacationTimeType.map((vacationTime, idx) => {
+                                    return (
+                                        <div className="float-start" key={'vacationTimeType' + idx}>
+                                            <input 
+                                                type="radio" 
+                                                name="vacationTimeType"
+                                                value={vacationTime.value}
+                                                onClick={changeVacationTimeType}
+                                                disabled={disableSelectableVacationTime}
+                                            />
+                                            <label htmlFor={vacationTime.name}>{vacationTime.name}</label>
+                                        </div>
+                                        )
+                                })} 
+                            </td>
+                        </tr>
+                        <tr>
                             <th scope="row" className="border border-gray-300 w-1/3">구분</th>
                             <td className="border border-gray-300 w-2/3">
-                        {vacationRangeType.map((vacationRange, idx) => {
-                            return (
-                                <div className="float-start" key={'vacationRangeType' + idx}>
-                                    <input 
-                                        type="radio" 
-                                        name="vacationRangeType"
-                                        value={vacationRange.value}
-                                        onClick={changeVacationRangeType}
-                                    />
-                                    <label htmlFor={vacationRange.name}>{vacationRange.name}</label>
-                                </div>
-                                )
-                            })}
-                    </td>
-                </tr>
-            </table>
+                                {vacationRangeType.map((vacationRange, idx) => {
+                                    return (
+                                        <div className="float-start" key={'vacationRangeType' + idx}>
+                                            <input 
+                                                type="radio" 
+                                                name="vacationRangeType"
+                                                value={vacationRange.value}
+                                                onClick={changeVacationRangeType}
+                                            />
+                                            <label htmlFor={vacationRange.name}>{vacationRange.name}</label>
+                                        </div>
+                                        )
+                                    })}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row" className="border border-gray-300 w-1/3">근태 일자</th>
+                            <td className="border border-gray-300 w-2/3">
+                                <input 
+                                    type="date"
+                                    name="vacationDate" 
+                                    onChange={changeVacationDate}/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row" className="border border-gray-300 w-1/3">신청자명</th>
+                            <td className="border border-gray-300 w-2/3">
+                                <input 
+                                    name="vacationUserName" 
+                                    onChange={changeVacationUserName}/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colSpan={2} className="text-center">
+                                <button type="submit" > 신청 </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </form>
+            </div>
+            <div className="flex w-128 overflow-hidden"></div>
         </div>
     )
 }
